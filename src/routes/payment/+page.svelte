e524fc66082327d6b15f535a060e7994aa860f24
HEMANT PATHAK <122811374+hemant030406@users.noreply.github.com>
2023-12-15 09:35:44 -0800

    mobile version of payment and sponsor page

--- src/routes/payment/+page.svelte
+++ src/routes/payment/+page.svelte
@@ -1,15 +1,4 @@
 <script lang="ts">
-	import Icon from 'svelte-awesome/components/Icon.svelte';
-	import {
-		user,
-		shoppingCart,
-		mapMarker,
-		calendar,
-		instagram,
-		facebook,
-		linkedin,
-		twitter
-	} from 'svelte-awesome/icons';
 	import { onMount } from 'svelte';
 	import QRCode from 'qrcode';
 
@@ -36,7 +25,7 @@
 	});
 
 	const verify = (CAcode: string) => {
-		let warning = (document.getElementById('warning'));
+		let warning = document.getElementById('warning');
 		if (CAcode.length !== 6) {
 			if (warning != null) {
 				warning.style.visibility = 'visible';
@@ -44,8 +33,8 @@
 			}
 			setTimeout(() => {
 				if (warning != null) {
-				warning.style.visibility = 'hidden';
-			}
+					warning.style.visibility = 'hidden';
+				}
 			}, 3500);
 		} else {
 			fetch('https://testpetri.onrender.com/api/events/verify', {
@@ -59,11 +48,11 @@
 			})
 				.then((response) => response.json())
 				.then((data) => {
-					console.log(data)
-					console.log('a')
+					console.log(data);
+					console.log('a');
 					success = data.verified;
 					let verify = window.document.getElementById('verify');
-					let CAcodeElt = window.document.getElementById('CAcode')  as HTMLInputElement;
+					let CAcodeElt = window.document.getElementById('CAcode') as HTMLInputElement;
 					if (success) {
 						amount = 1000;
 						qrcodeurl = `upi://pay?pa=${accountNumber}@${ifscCode}.ifsc.npci&am=${amount}&cu=INR`;
@@ -77,22 +66,21 @@
 							bgimage.style.backgroundImage = `url(${imgurl})`;
 						}
 
-						if(verify != null){
+						if (verify != null) {
 							verify.innerHTML = 'Verified';
 							CAcodeElt.disabled = true;
 						}
-					}
-					else{
-						if(verify != null){
+					} else {
+						if (verify != null) {
 							verify.innerHTML = 'Unverified';
 							verify.style.backgroundColor = 'red';
 						}
-						setTimeout(()=>{
-							if(verify != null){
+						setTimeout(() => {
+							if (verify != null) {
 								verify.innerHTML = 'Verify';
 								verify.style.backgroundColor = 'rgb(17, 184, 17)';
 							}
-						},2000)
+						}, 2000);
 					}
 				});
 		}
@@ -121,108 +109,108 @@
 			}
 			setTimeout(() => {
 				if (warning != null) {
-				warning.style.visibility = 'hidden';
-			}
+					warning.style.visibility = 'hidden';
+				}
 			}, 3500);
 		}
 	};
 </script>
 
-<div id='all'>
-<h1 style="text-align:center;margin-top:10rem;">
-	Registering for <span style="color: blueviolet;">{data.name}</span>
-</h1>
+<div id="all">
+	<h1 style="text-align:center;margin-top:10rem;">
+		Registering for <span style="color: blueviolet;">{data.name}</span>
+	</h1>
 
-<div class="form">
-	<div style="background-color: rgb(90, 14, 137,0.3);" class="payment">
-		<div style="display: inline-block;">
-			<p
-				style="margin-bottom: 1rem;margin-top: 0.8rem;font-size:155%;display: inline-block;font-family:monospace"
-			>
-				Scan here for payment
-			</p>
-			<div id="img" />
-			<p style="margin-top: 1rem;font-size:155%;box-sizing:border-box;font-family:cursive">
-				<span style="font-family:cursive">&#8377;</span>{amount}
-			</p>
-			<svg
-				style="margin-top: -1rem;"
-				xmlns="http://www.w3.org/2000/svg"
-				width="120"
-				height="50"
-				fill-rule="evenodd"
-				><path d="M95.678 42.9L110 29.835l-6.784-13.516z" fill="#097939" /><path
-					d="M90.854 42.9l14.322-13.065-6.784-13.516z"
-					fill="#ed752e"
-				/><path
-					d="M22.41 16.47l-6.03 21.475 21.407.15 5.88-21.625h5.427l-7.05 25.14c-.27.96-1.298 1.74-2.295 1.74H12.31c-1.664 0-2.65-1.3-2.2-2.9l6.724-23.98zm66.182-.15h5.427l-7.538 27.03h-5.58zM49.698 27.582l27.136-.15 1.81-5.707H51.054l1.658-5.256 29.4-.27c1.83-.017 2.92 1.4 2.438 3.167L81.78 29.49c-.483 1.766-2.36 3.197-4.19 3.197H53.316L50.454 43.8h-5.28z"
-					fill="#747474"
-				/></svg
-			>
+	<div class="form">
+		<div style="background-color: rgb(90, 14, 137,0.3);" class="payment">
+			<div style="display: inline-block;">
+				<p
+					style="margin-bottom: 1rem;margin-top: 0.8rem;font-size:155%;display: inline-block;font-family:monospace"
+				>
+					Scan here for payment
+				</p>
+				<div id="img" />
+				<p style="margin-top: 1rem;font-size:155%;box-sizing:border-box;font-family:cursive">
+					<span style="font-family:cursive">&#8377;</span>{amount}
+				</p>
+				<svg
+					style="margin-top: -1rem;"
+					xmlns="http://www.w3.org/2000/svg"
+					width="120"
+					height="50"
+					fill-rule="evenodd"
+					><path d="M95.678 42.9L110 29.835l-6.784-13.516z" fill="#097939" /><path
+						d="M90.854 42.9l14.322-13.065-6.784-13.516z"
+						fill="#ed752e"
+					/><path
+						d="M22.41 16.47l-6.03 21.475 21.407.15 5.88-21.625h5.427l-7.05 25.14c-.27.96-1.298 1.74-2.295 1.74H12.31c-1.664 0-2.65-1.3-2.2-2.9l6.724-23.98zm66.182-.15h5.427l-7.538 27.03h-5.58zM49.698 27.582l27.136-.15 1.81-5.707H51.054l1.658-5.256 29.4-.27c1.83-.017 2.92 1.4 2.438 3.167L81.78 29.49c-.483 1.766-2.36 3.197-4.19 3.197H53.316L50.454 43.8h-5.28z"
+						fill="#747474"
+					/></svg
+				>
+			</div>
 		</div>
-	</div>
-	<div id="data">
-		<p style="color: #FCF3FF;margin-left:1rem;margin-bottom:0.5rem">Transaction Id</p>
-		<input
-			id="transId"
-			type="text"
-			maxlength="25"
-			placeholder="Your Transaction Id"
-			style="display:block;"
-			bind:value={transactionID}
-			required
-		/>
-		<p style="color: #FCF3FF;margin-left:1rem;margin-bottom:0.5rem">CA Code</p>
-		<div class="code_verification" style="">
+		<div id="data">
+			<p style="color: #FCF3FF;margin-left:1rem;margin-bottom:0.5rem">Transaction Id</p>
 			<input
-				id="CAcode"
+				id="transId"
 				type="text"
-				placeholder="Your CA Code(if any)"
-				maxlength="6"
-				style="margin-top:5px;background-color:black !important;border:black"
-				bind:value={CAcode}
+				maxlength="25"
+				placeholder="Your Transaction Id"
+				style="display:block;"
+				bind:value={transactionID}
+				required
 			/>
-			<button id="verify" style="cursor:pointer" on:click={() => verify(CAcode)}
-				>Verify</button
-			>
-		</div>
-		<div id="submitButton">
-			<p id="warning">Hii</p>
-			<input
-				id="submit"
-				on:click={() => submit()}
-				type="submit"
-				style="cursor:pointer; display:block; margin-top:5px;"
-			/>
+			<p style="color: #FCF3FF;margin-left:1rem;margin-bottom:0.5rem">CA Code</p>
+			<div class="code_verification" style="">
+				<input
+					id="CAcode"
+					type="text"
+					placeholder="Your CA Code(if any)"
+					maxlength="6"
+					style="margin-top:5px;background-color:black !important;border:black"
+					bind:value={CAcode}
+				/>
+				<button id="verify" style="cursor:pointer" on:click={() => verify(CAcode)}>Verify</button>
+			</div>
+			<div id="submitButton">
+				<p id="warning">Hii</p>
+				<input
+					id="submit"
+					on:click={() => submit()}
+					type="submit"
+					style="cursor:pointer; display:block; margin-top:5px;"
+				/>
+			</div>
 		</div>
 	</div>
-</div>
 </div>
 
 <style>
-	#all{
+	/* #all {
 		display: block;
-	}
+	} */
 	.form {
-		margin-left: 10rem;
+		width: 100vw;
+		grid-template-areas: 'data payment';
+		grid-column-gap: 3rem;
 		margin-top: 4rem;
 		padding-top: 0rem;
 		margin-bottom: 10rem;
-		
+		display: grid;
+		align-items: center;
+		justify-content: center;
 	}
 	.payment {
-		margin-right: 15rem;
+		grid-area: payment;
 		height: 23rem;
 		width: 20rem;
 		text-align: center;
 		background-color: black;
-		float: right;
 		padding: 2.5rem;
 		padding-top: 0.7rem;
 		border-radius: 10%;
 		z-index: 1;
 		position: relative;
-		margin-top: 0rem;
 	}
 	* {
 		box-sizing: border-box;
@@ -291,7 +279,6 @@
 		margin-right: 1rem;
 		margin-left: 3rem;
 		border-radius: 20%/50%;
-		/* line-height: 1.5rem; */
 	}
 	#CAcode {
 		width: 18rem;
@@ -301,16 +288,43 @@
 		outline: none;
 	}
 	input:-webkit-autofill,
-	input:-webkit-autofill:hover, 
-	input:-webkit-autofill:focus{
+	input:-webkit-autofill:hover,
+	input:-webkit-autofill:focus {
 		transition: background-color 5000s ease-in-out 0s;
 		-webkit-text-fill-color: #fcf3ff;
 	}
 	#data {
+		grid-area: data;
 		width: 30rem;
 		display: inline-block;
 		z-index: 2;
 		margin-top: 1rem;
-		margin-left: 10rem;
+	}
+
+	@media screen and (max-width: 900px) {
+		h1 {
+			width: 100vw;
+		}
+		.form {
+			width: 100vw;
+			position: relative;
+			left: 50%;
+			right: 50%;
+			margin-left: -50vw;
+			margin-right: -50vw;
+			grid-template-areas:
+				'payment'
+				'data';
+			grid-column-gap: 3rem;
+			margin-top: 4rem;
+			padding-top: 0rem;
+			margin-bottom: 10rem;
+			display: grid;
+			align-items: center;
+			/* padding-left: 35%; */
+		}
+		#data {
+			margin-left: -3rem;
+		}
 	}
 </style>
--- src/routes/sponsors/+page.svelte
+++ src/routes/sponsors/+page.svelte
@@ -67,7 +67,7 @@
 		<div class="name">Royal Enfield</div>
 	</div>
 </div>
-<div style="height: 5rem;"></div>
+<div style="height: 5rem;" />
 
 <style>
 	#pnbSponsor {
@@ -97,11 +97,12 @@
 		grid-template-areas:
 			'pnb trip sbi'
 			'canara sparx RF';
-        grid-row-gap : 5rem;
-        grid-column-gap : 10rem;
+		grid-row-gap: 5rem;
+		grid-column-gap: 10rem;
 		align-items: center;
 		justify-content: center;
 	}
+
 	#heading1 {
 		margin-top: 8rem;
 		margin-bottom: 4rem;
@@ -111,7 +112,7 @@
 		margin: 4rem;
 		text-align: center;
 	}
-    
+
 	#pnbSponsor > .title {
 		width: 200px;
 		display: flex;
@@ -136,7 +137,7 @@
 		width: 200px;
 	}
 	#tripshopinSponsor > .title {
-		width:200px;
+		width: 200px;
 		display: flex;
 		justify-content: center;
 		align-items: center;
@@ -249,5 +250,216 @@
 		background-position: center;
 		height: 200px;
 		width: 200px;
+	}
+
+	/* Mobile Version */
+	@media screen and (max-width: 960px) and (min-width: 375px) {
+		#all {
+			display: grid;
+			grid-template-areas:
+				'pnb trip'
+				'sbi canara'
+				'sparx RF';
+			grid-row-gap: 5rem;
+			grid-column-gap: 10rem;
+			align-items: center;
+			justify-content: center;
+			margin: auto;
+			margin-right: -100%;
+		}
+
+		#heading1 {
+			margin-top: 8rem;
+			margin-bottom: 4rem;
+			text-align: center;
+			width: 200%;
+		}
+		#heading2 {
+			margin: 4rem;
+			text-align: center;
+			width: 175%;
+		}
+	}
+	@media screen and (max-width: 375px) {
+		#all {
+			display: grid;
+			grid-template-areas:
+				'pnb'
+				'trip'
+				'sbi'
+				'canara'
+				'sparx'
+				'RF';
+			grid-row-gap: 5rem;
+			grid-column-gap: 10rem;
+			align-items: center;
+			justify-content: center;
+			margin: auto;
+			margin-right: -300%;
+		}
+
+		#heading1 {
+			margin-top: 8rem;
+			margin-bottom: 4rem;
+			text-align: center;
+			width: 400%;
+			font-size: 300%;
+		}
+		#heading2 {
+			margin: 4rem;
+			text-align: center;
+			font-size: 300%;
+			width: 360%;
+		}
+		#pnbSponsor > .title {
+			width: 400px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			font-size:200%
+		}
+		#pnbSponsor > .name {
+			width: 400px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			font-size:200%
+		}
+		#pnbSponsor > .img {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			background-image: url('https://static.vecteezy.com/system/resources/previews/020/190/451/original/punjab-national-bank-pnb-bank-logo-free-free-vector.jpg');
+			background-repeat: no-repeat;
+			background-size: contain;
+			background-position: center;
+			height: 400px;
+			width: 400px;
+		}
+		#tripshopinSponsor > .title {
+			width: 400px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			font-size:200%
+		}
+		#tripshopinSponsor > .name {
+			width: 400px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			font-size:200%
+		}
+		#tripshopinSponsor > .img {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			background-image: url('https://play-lh.googleusercontent.com/a-/ALV-UjWfkWfpYBLvjRx6321IKEFUaVGftxHt2QXS6bTyR4Z260Js');
+			background-repeat: no-repeat;
+			background-size: contain;
+			background-position: center;
+			height: 400px;
+			width: 400px;
+		}
+		#sbiSponsor > .title {
+			width: 400px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			font-size:200%
+		}
+		#sbiSponsor > .name {
+			width: 400px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			font-size:200%
+		}
+		#sbiSponsor > .img {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			background-image: url('https://www.logo-designer.co/storage/2017/04/2017-Design-Stack-new-logo-design-State-Bank-of-India.png');
+			background-repeat: no-repeat;
+			background-size: contain;
+			background-position: center;
+			height: 400px;
+			width: 400px;
+		}
+		#canaraSponsor > .title {
+			width: 400px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			font-size:200%
+		}
+		#canaraSponsor > .name {
+			width: 400px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			font-size:200%
+		}
+		#canaraSponsor > .img {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			background-image: url('https://st.adda247.com/https://adda247-wp-multisite-assets.s3.ap-south-1.amazonaws.com/wp-content/uploads/multisite/sites/5/2022/07/26075007/Canara-Bank-Limited-Logo.jpg');
+			background-repeat: no-repeat;
+			background-size: contain;
+			background-position: center;
+			height: 400px;
+			width: 400px;
+		}
+		#sparxSponsor > .title {
+			width: 400px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			font-size:200%
+		}
+		#sparxSponsor > .name {
+			width: 400px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			font-size:200%
+		}
+		#sparxSponsor > .img {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			background-image: url('https://seeklogo.com/images/S/sparx-logo-B9ECFE3A4B-seeklogo.com.png');
+			background-repeat: no-repeat;
+			background-size: contain;
+			background-position: center;
+			height: 400px;
+			width: 400px;
+		}
+		#RFSponsor > .title {
+			width: 400px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			font-size:200%
+		}
+		#RFSponsor > .name {
+			width: 400px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			font-size:200%
+		}
+		#RFSponsor > .img {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			background-image: url('https://ih1.redbubble.net/image.4768528822.6719/st,small,507x507-pad,600x600,f8f8f8.jpg');
+			background-repeat: no-repeat;
+			background-size: contain;
+			background-position: center;
+			height: 400px;
+			width: 400px;
+		}
 	}
 </style>
