3008458e5a9ed73641a3e25380169111ec56cd32
enchanteddev <119434133+enchanteddev@users.noreply.github.com>
2023-12-18 23:53:07 +0530

    /api/ -> /web/ login server connection

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,9 +1,9 @@
 const backend_url = 'http://127.0.0.1:8000/'
 
 export const API = {
-    login: backend_url + 'api/login/',
-    register: backend_url + 'api/register/',
-    logout: backend_url + 'api/logout/',
-    events_apply: backend_url + 'api/events/apply/',
-    feedback: backend_url + 'api/send_grievance',
+    login: backend_url + 'web/login/',
+    register: backend_url + 'web/register/',
+    logout: backend_url + 'web/logout/',
+    events_apply: backend_url + 'web/events/apply/',
+    feedback: backend_url + 'web/send_grievance',
 }
