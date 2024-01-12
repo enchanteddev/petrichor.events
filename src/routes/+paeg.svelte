5ce8a536a7f1314c8264ae092daf25050a1e8198
enchanteddev <119434133+enchanteddev@users.noreply.github.com>
2024-01-12 15:31:33 +0530

    maintenacnce

---
+++ src/routes/+paeg.svelte
@@ -0,0 +1,43 @@
+<main>
+    <div>
+        <h1>Maintainence!</h1>
+        <h1>Maintainence!</h1>
+        <h1>Maintainence!</h1>
+        <h1>Maintainence!</h1>
+        <h1>Maintainence!</h1>
+        <h1>Maintainence!</h1>
+        <h1>Maintainence!</h1>
+        <h1>Maintainence!</h1>
+        <h1>Maintainence!</h1>
+        <h1>Maintainence!</h1>
+    </div>
+</main>
+<style>
+    main{
+        display: grid;
+        place-items: center;
+        height: 100vh;
+        font-size: 30px;
+    }
+    h1{
+        width: 30rem;
+        margin-inline: 0.5rem;
+    }
+    div{
+        position: absolute;
+        display: flex;
+        animation: loop 2s linear infinite;
+        border-width: 10px 0px 10px 0px;
+        border-style: solid;
+        border-color: white;
+    }
+
+    @keyframes loop{
+        from{
+            left: 0
+        }
+        to{
+            left: -31rem;
+        }
+    }
+</style>
