a07b1eb6d93b5f1b713455af48a6d4405f8e59bd
enchanteddev <119434133+enchanteddev@users.noreply.github.com>
2024-01-14 18:16:24 +0530

    accom HTML from lst yr

---
+++ src/routes/accomodation/+page.svelte
@@ -0,0 +1,114 @@
+<div class="flex justify-center items-center h-screen bg-blue-lightest ayush-acc-whole">
+	<div class="je_acco_banger_title_contaier">
+		<h class="merch-head">Accomodations</h>
+	</div>
+
+	<div class="abt_para_2 abt_para" data-aos="zoom-in">
+		<div class="jj_acco_pic_container_button">
+			<a class="jj_clean_a" href="https://forms.gle/idhAfBGCPjvWDmNJ8" target="_blank">
+				{' '}
+				Book Now{' '}
+			</a>
+		</div>
+		<br />
+		<br />
+		<br />
+		<br />
+		<div class="row justify-content-center">
+			<div class="col-lg-8 col-10 col-md-9">
+				Petrichor '24 - IIT Palakkad | Events | Proshow | Workshops | Open Day
+				<ul>
+					<li>Accommodation is arranged at external hotels.</li>
+					<li>Below mentioned rates are per day rates.</li>
+					<li>This form is only for accommodation.</li>
+					<li>
+						Food stalls will be available in campus, participants can purchase food of their choice.
+					</li>
+				</ul>
+				<br />
+				<i>Hotel Name: Orma Tourist Home,</i>
+				<br />
+				<i> Location: Opposite Palakkad Jn Railway Station, Olavakkode, Palakkad </i>
+				<br />
+				<i>(14kms from IIT Palakkad Nila Campus, Kanjikode)</i>
+				<br />
+				<br />
+				Terms & conditions:
+				<br />
+				<strong>Note:</strong>
+				<br />
+				<br />
+				<ul>
+					<strong>
+						<ul>
+							<li>Rate per head</li>
+						</ul>
+						<ul>
+							<li>Rs 380 for one day</li>
+							<li>Rs 680 for two days</li>
+							<li>Rs 930 for three days</li>
+						</ul>
+					</strong>
+					<strong>
+						<li>Rooms will be on sharing basis with other participants of the fest.</li>
+					</strong>
+					<strong>
+						<li>
+							If you are travelling in a group, fill names and make payment together in one single
+							form for allocating rooms together.
+						</li>
+					</strong>
+					<strong>
+						<li>Seperate forms to be filled for group of boys & girls.</li>
+					</strong>
+					<strong>
+						<li>
+							Check-Out time will be 24hrs from Check-in time if payment is done for one day. If
+							exceeding 24hrs, payment for the next day has to be done.
+						</li>
+					</strong>
+				</ul>
+				<ol>
+					<li>Accommodation once booked cannot be cancelled & the amount will NOT be refunded.</li>
+					<li>
+						ONLY the participants whose names are registered in this form will be allowed into the
+						rooms. There will be an allocation list at the hotel for verifying the same.
+					</li>
+					<li>
+						Participants MUST carry a valid govt. ID card (aadhar card/ driving licence/ Ration
+						card/ Voter Id etc... Pancard not accepted) for verification at the hotel.
+					</li>
+					<li>
+						If the room is not vacated before the corresponding check-out time, an additional amount
+						for the next day has to be paid by the participants according to the hotel policies.
+					</li>
+					<li>
+						While checking out, your luggage should also be carried with you and the key must be
+						handed over at the reception. Luggages should not be left at the hotel, and failing to
+						submit the keys will lead to additional payment.
+					</li>
+					<li>
+						We do not recommend carrying anything expensive. Safety of your belongings is the
+						responsibility of the participant.
+					</li>
+					<li>
+						The amount collected in this form is ONLY for accommodation. This rate does not include
+						transportation.
+					</li>
+				</ol>
+				<ul>
+					<li>
+						Timings of all the events and workshops will be shared by respective organizers before
+						the fest. Participants are expected to reach the campus on their own before the event
+						timings
+					</li>
+				</ul>
+			</div>
+		</div>
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+	</div>
+</div>
