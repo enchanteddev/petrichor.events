f5b0b39c58342a1117f43ea625fa24240635a826
Chirag Varshney <134807686+Chirag9528@users.noreply.github.com>
2024-01-22 18:29:53 +0530

    Update data.js

--- src/lib/data.js
+++ src/lib/data.js
@@ -61,7 +61,7 @@ import Chess from '$lib/assets/Informals/Chess.jpg'
 
 
 export const closedRegistrations = [
-    "TF07", "TF08", "TF03" ,"TF00" , "TF01 , TF10" , "TF11"
+    "TF07", "TF08", "TF03" ,"TF00" , "TF01 , TF10" , "TF11" , "TF10"
 ]
 
 export let cultural = {
