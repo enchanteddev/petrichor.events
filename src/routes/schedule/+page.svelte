5a431aca9a30a5e7ef68a13e08d4af0fe946c50b
enchanteddev <119434133+enchanteddev@users.noreply.github.com>
2024-01-26 03:44:53 +0530

    added tangle

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -1,11 +1,19 @@
 <script>
     import {schedule} from '$lib/sched.json'
 	import {getEventDataJS} from '$lib/helper'
-
+    import tangle from '$lib/assets/Sponsors/Tangle.jpg'
     import Schedule from '$lib/components/Schedule.svelte';
 
 
 </script>
+
+<div class="overlay">
+    <div class="container">
+        <h1 style="text-align: center;">View The Schedule on <span class="atmos">Tangle</span></h1>
+        <img src={tangle} alt="">
+        <a href='https://play.google.com/store/apps/details?id=com.tangleoffline.tangle&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>
+    </div>
+</div>
 
 <div class="main">
 
@@ -24,11 +32,36 @@
 </div>
 
 <style>
+    .overlay{
+        position: fixed;
+        top: 0;
+        left: 0;
+        z-index: 1;
+        height: 100svh;
+        width: 100svw;
+        display: grid;
+        place-items: center;
+    }
+    .container{
+        display: flex;
+        flex-direction: column;
+        place-items: center;
+    }
+    .container > img{
+        width: 20em;
+        display: block;
+    }
+    a>img{
+        width: 23em;
+    }
     .main{
         display: flex;
         flex-direction: column;
         place-items: center;
         margin-top: 7em;
+        filter: blur(15px);
+        height: 100svh;
+        overflow: hidden;
     }
     .grid{
         display: grid;
